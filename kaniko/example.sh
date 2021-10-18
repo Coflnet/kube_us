d6f6ccff02d936899cd721c559b2182375df06d6
Flou21 <florian.muehlhans@icloud.com>
2021-10-18 18:17:01 +0200

    remove other stuff

--- kaniko/example.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-docker run gcr.io/kaniko-project/executor:latest \
-    --context=git://github.com/Coflnet/hypixel-react.git#refs/heads/master \
-    --context-sub-path=. \
-    --no-push
-    #--destination=flou21/example-frontend
